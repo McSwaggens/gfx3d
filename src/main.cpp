f272dba39cbb503996134cebaceb9cd19ed72bb2
Daniel Jones <McSwaggens@protonmail.com>
2018-12-21 18:59:22 +1100

    Changed background color to black

--- src/main.cpp
+++ src/main.cpp
@@ -208,7 +208,7 @@ void logic()
 
 void render()
 {
-	glClearColor(0.2f, 0.2f, 0.6f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// Draw quad
