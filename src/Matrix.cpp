ae53e42090bb26cd4a87b045295c5fed764a4a63
Daniel Jones <McSwaggens@protonmail.com>
2019-02-02 14:10:04 +1100

    Added Matrix

---
+++ src/Matrix.cpp
@@ -0,0 +1,48 @@
+#include "Matrix.h"
+#include <iostream>
+
+void MatrixTests()
+{
+	Matrix<2, 2> a =
+	{
+	//  X Y
+		1, 3, // X
+		2, -1 // Y
+	};
+
+	Matrix<2, 2> b =
+	{
+	//  X Y
+		2, -4, // X
+		3, 0   // Y
+	};
+
+	std::cout << "a: " << a;
+	std::cout << "b: " << b;
+	std::cout << "a*b: " << (a * b);
+
+	Matrix<4, 4> c =
+	{
+	//  X  Y  Z  W
+		1, 0, 0, 0, // X
+		0, 1, 0, 0, // Y
+		0, 0, 1, 0, // Z
+		0, 0, 0, 1  // W
+	};
+
+	Vector d { 1, 1, 1, 1 };
+	std::cout << "c: " << c;
+	std::cout << "c*" << d << " = " << (c * d) << std::endl;
+
+	Matrix<2, 2> e =
+	{
+	//  X Y
+		0, 2, // X
+		4, 0 // Y
+	};
+
+	std::cout << "a*" << d << " = " << (a * d) << std::endl;
+	std::cout << "b*" << d << " = " << (b * d) << std::endl;
+	std::cout << std::endl << "e: " << e;
+	std::cout << "e*" << d << " = " << (e * d) << std::endl;
+}
---
+++ src/Matrix.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "Vector.h"
+#include <iostream>
+#include <iomanip>
+
+/*
+ * M = Rows
+ * N = Columns
+ */
+
+template<size_t N, size_t M>
+struct Matrix final
+{
+	float a[M * N];
+
+	Matrix<N, M> operator * (const Matrix<N, M>& z)
+	{
+		Matrix w = { 0 };
+		for (size_t m = 0; m < M; m++)
+		{
+			for (size_t n = 0; n < N; n++)
+			{
+				for (size_t l = 0; l < M && l < N; l++)
+				{
+					w.a[m * M + n] += a[m * M + l] * z.a[l * M + n];
+				}
+			}
+		}
+		return w;
+	}
+
+	Vector operator * (const Vector& v)
+	{
+		Vector w;
+		for (size_t m = 0; m < M && m < 4; m++)
+		{
+			for (size_t l = 0; l < N && l < 4; l++)
+			{
+				w.a[m] += a[m * M + l] * v.a[l];
+			}
+		}
+		return w;
+	}
+
+	friend std::ostream& operator << (std::ostream& os, const Matrix& x)
+	{
+		static const char g_dimension_symbols[] = "XYZW";
+		os << std::fixed << std::setprecision(2);
+
+		for (size_t n = 0; n < N && n < 4; n++)
+		{
+			os << "\t" << g_dimension_symbols[n] << ":";
+		}
+		os << std::endl;
+
+		for (size_t m = 0; m < M && m < 4; m++)
+		{
+			os << "    " << g_dimension_symbols[m] << ": ";
+			for (size_t n = 0; n < N && n < 4; n++)
+			{
+				os << "\t" << x.a[m * M + n];
+			}
+			os << std::endl;
+		}
+		os << std::endl;
+		return os;
+	}
+};
+
+void MatrixTests();
+
