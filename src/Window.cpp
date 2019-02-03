d6aead0a94d1104afcd7bfa1270236c23fcb5085
Daniel Jones <McSwaggens@protonmail.com>
2019-02-03 20:47:31 +1100

    Added Window resolution and ratio utility functions

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,19 +1,47 @@
 #include "Window.h"
 
-void Framebuffer_SizeChanged_Callback(GLFWwindow* g_window, int width, int height)
+void Framebuffer_SizeChanged_Callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+void Window_SizeChanged_Callback(GLFWwindow* window, int width, int height)
+{
+	// FIXME: Unused because there isn't a very-
+	// 	good way to get the propper Window class.
 }
 
 Window::Window(const std::string& name, uint32_t width, uint32_t height)
 {
 	m_window = glfwCreateWindow(width, height, name.c_str(), nullptr, nullptr);
 	glfwSetFramebufferSizeCallback(m_window, Framebuffer_SizeChanged_Callback);
+	glfwSetWindowSizeCallback(m_window, Window_SizeChanged_Callback);
+}
+
+Window::Resolution Window::GetResolution() const
+{
+	Window::Resolution res;
+	glfwGetWindowSize(m_window, &res.width, &res.height);
+	return res;
+}
+
+double Window::WidthInHeight() const
+{
+	Window::Resolution res = GetResolution();
+	return (double)res.width / (double)res.height;
+}
+
+double Window::HeightInWidth() const
+{
+	return 1 / WidthInHeight();
 }
 
 Window::~Window()
 {
-	glfwDestroyWindow(m_window);
+	if (m_window)
+	{
+		glfwDestroyWindow(m_window);
+	}
 }
 
 void Window::SwapBuffers()
--- src/Window.h
+++ src/Window.h
@@ -11,6 +11,16 @@ public:
 	Window(const std::string& name, uint32_t width, uint32_t height);
 	~Window();
 
+	struct Resolution
+	{
+		int32_t width;
+		int32_t height;
+	};
+
+	Resolution GetResolution() const;
+	double HeightInWidth() const;
+	double WidthInHeight() const;
+
 	void SwapBuffers();
 	void BindContext();
 	GLFWwindow* GetGLFWWindow();
